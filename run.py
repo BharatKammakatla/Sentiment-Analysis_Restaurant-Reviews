8c3dcb8f250f3a08ba2c5c68ae9da56da65d551d
Bharat Kammakatla <bharatkammakatla@gmail.com>
2020-08-04 16:57:08 +0800

    Working version

---
+++ run.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+from flask_bootstrap import Bootstrap
+import pickle
+
+# Load the Multinomial Navie Bayes and TFID vectorizer object from local
+classifier = pickle.load(open('restaurant-sentiment-mnb-model.pkl', 'rb'))
+tf = pickle.load(open('tf-transform.pkl', 'rb'))
+
+bootstrap = Bootstrap()
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        message = request.form['message']
+        data = [message]
+        tf_vect = tf.transform(data).toarray()
+        prediction = classifier.predict(tf_vect)
+        return render_template('result.html', prediction=prediction)
+
+
+if __name__ == '__main__':
+    bootstrap.init_app(app)  # Initialize bootstrap
+    app.run(debug=True)
